20e22f61170e6ac74fc073ec13c9e8fd5a4e1838
Francesco215 <f.sacco@protonmail.com>
2022-04-14 21:33:01 +0200

    numerazione sezioni

---
+++ website/new_types.js
@@ -0,0 +1,75 @@
+function createCustomLink(anchorText, anchorLink){
+    var aTag = document.createElement("a");
+    aTag.href = anchorLink;
+    aTag.innerHTML = anchorText;
+
+    return aTag ;
+}
+
+function createCustomText(anchorText, type="h2"){
+    var Class;
+    if (type=="h2") Class = "section";
+    if (type=="h3") Class = "subsection";
+
+    var tag = document.createElement(type);
+    tag.innerHTML = anchorText;
+    tag.setAttribute("class", Class);
+
+    return tag ;
+}
+
+//this code defines and manages the sections
+class Sec extends HTMLElement {
+    connectedCallback() {
+        var text=this.textContent;
+        this.textContent="";
+        this.appendChild(createCustomText(text,"h2"));
+
+    };
+}
+customElements.define('d-section', Sec);
+
+
+//this code defines and manages the sub-sections
+class SubSec extends HTMLElement {
+    connectedCallback() {
+        var text=this.textContent;
+        this.textContent="";
+        this.appendChild(createCustomText(text,"h3"));
+
+    };
+}
+customElements.define('d-subsection', SubSec);
+
+
+
+
+
+// this is to assign the numbers to the sections
+var sections=document.querySelectorAll('.section,.subsection');
+var n_section=0;
+var n_subsection=0;
+for (var i=0;i<sections.length;i++){
+    var parent=sections[i].parentElement;
+    if (sections[i].className == "section"){
+        n_section++;
+        n_subsection=0;
+        parent.setAttribute("number",n_section);
+    }
+    if (sections[i].className=="subsection"){
+        n_subsection++;
+        parent.setAttribute("number",n_section+"."+n_subsection);
+    }
+
+}
+
+
+//this code manages the references
+class Ref extends HTMLElement {
+    connectedCallback() {
+        var key=this.getAttribute("key");
+        var number= document.getElementById(key).getAttribute('number');
+        this.appendChild(createCustomLink(number,"#"+key));
+    };
+}
+customElements.define('d-reference', Ref);
